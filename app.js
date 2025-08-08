0ed19fcc9269f726f62eea65777e622a89b01244
Denis Alexandre Furtado <kddenisfurtado@gmail.com>
2025-08-08 13:24:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.5
+    },
+    "size": {
+      "value": 3
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js carregado com sucesso!');
+});
